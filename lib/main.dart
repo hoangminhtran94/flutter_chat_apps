c4398b2c5d73e181eabd07dc3d2c619c9e5d8378
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-09-13 21:31:49 -0400

    adding friends screen, update chatbox

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class App extends StatelessWidget {
         theme: ThemeData().copyWith(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
-              seedColor: const Color.fromARGB(255, 63, 17, 177)),
+              seedColor: const Color.fromARGB(255, 124, 7, 202)),
         ),
         home: StreamBuilder(
           builder: (context, snapshot) {
--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,14 +1,44 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_apps/widgets/chat_messages.dart';
 import 'package:flutter_chat_apps/widgets/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+
+    final token = await fcm.getToken();
+    print(token);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: "Chats"),
+          BottomNavigationBarItem(icon: Icon(Icons.people), label: "Friends"),
+          BottomNavigationBarItem(icon: Icon(Icons.timer), label: "History"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings")
+        ],
+        backgroundColor: Colors.grey,
+      ),
       appBar: AppBar(
         title: Text(
           "FlutterChat",
